3b4cd0396f2d75a2b64517f6a5d60a09d2575e4e
Nicolas Ramz <nicolas.ramz@gmail.com>
2012-10-17 10:33:58 +0200

    CLEANED-UP: brackets/comments/console.log

--- js/deferred.js
+++ js/deferred.js
@@ -24,25 +24,26 @@
 			done: function() {
 				for (var i = 0; i < arguments.length; i++) {
 					// skip any undefined or null arguments
-					if (!arguments[i])
+					if (!arguments[i]) {
 						continue;
+					}
 
 					if (isArray(arguments[i])) {
 						var arr = arguments[i];
 						for (var j = 0; j < arr.length; j++) {
 							// immediately call the function if the deferred has been resolved
-							if (status === 'resolved')
+							if (status === 'resolved') {
 								arr[j].apply(this, resultArgs);
-								// exec(this, [arr[j]], this.resultArgs, this.status);
+							}
 
 							doneFuncs.push(arr[j]);
 						}
 					}
 					else {
 						// immediately call the function if the deferred has been resolved
-						if (status === 'resolved')
+						if (status === 'resolved') {
 							arguments[i].apply(this, resultArgs);
-							// exec(this, [arguments[i]], this.resultArgs, this.status);
+						}
 
 						doneFuncs.push(arguments[i]);
 					}
@@ -54,25 +55,26 @@
 			fail: function() {
 				for (var i = 0; i < arguments.length; i++) {
 					// skip any undefined or null arguments
-					if (!arguments[i])
+					if (!arguments[i]) {
 						continue;
+					}
 
 					if (isArray(arguments[i])) {
 						var arr = arguments[i];
 						for (var j = 0; j < arr.length; j++) {
 							// immediately call the function if the deferred has been resolved
-							if (status === 'rejected')
+							if (status === 'rejected') {
 								arr[j].apply(this, resultArgs);
-								// exec(this, [arr[j]], this.resultArgs, this.status);
+							}
 
 							failFuncs.push(arr[j]);
 						}
 					}
 					else {
 						// immediately call the function if the deferred has been resolved
-						if (status === 'rejected')
+						if (status === 'rejected') {
 							arguments[i].apply(this, resultArgs);
-							// exec(this, [arguments[i]], this.resultArgs, this.status);
+						}
 
 						failFuncs.push(arguments[i]);
 					}
@@ -88,21 +90,24 @@
 			progress: function() {
 				for (var i = 0; i < arguments.length; i++) {
 					// skip any undefined or null arguments
-					if (!arguments[i])
+					if (!arguments[i]) {
 						continue;
+					}
 
 					if (isArray(arguments[i])) {
 						var arr = arguments[i];
 						for (var j = 0; j < arr.length; j++) {
 							// immediately call the function if the deferred has been resolved
-							if (status == 'pending')
+							if (status === 'pending') {
 								progressFuncs.push(arr[j]);
+							}
 						}
 					}
 					else {
 						// immediately call the function if the deferred has been resolved
-						if (status === 'pending')
+						if (status === 'pending') {
 							progressFuncs.push(arguments[i]);
+						}
 					}
 				}
 				
@@ -111,16 +116,19 @@
 
 			then: function() {
 				// fail callbacks
-				if (arguments.length > 1 && arguments[1])
+				if (arguments.length > 1 && arguments[1]) {
 					this.fail(arguments[1]);
+				}
 
 				// done callbacks
-				if (arguments.length > 0 && arguments[0])
+				if (arguments.length > 0 && arguments[0]) {
 					this.done(arguments[0]);
+				}
 
 				// notify callbacks
-				if (arguments.length > 2 && arguments[2])
+				if (arguments.length > 2 && arguments[2]) {
 					this.progress(arguments[2]);
+				}
 			},
 
 			promise: function(obj) {
@@ -143,43 +151,49 @@
 			},
 
 			isRejected: function() {
-				return status == 'rejected';
+				return status === 'rejected';
 			},
 
 			isResolved: function() {
-				return status == 'resolved';
+				return status === 'resolved';
 			},
 
 			pipe: function(done, fail, progress) {
 				return D(function(def) {
 					foreach(done, function(func) {
 						// filter function
-						if (typeof func == 'function') {
+						if (typeof func === 'function') {
 							deferred.done(function() {
 								var returnval = func.apply(this, arguments);
 								// if a new deferred/promise is returned, its state is passed to the current deferred/promise
-								if (returnval && typeof returnval == 'function')
-									returnval.promise().then(def.resolve, def.reject, def.notify);									
-								else	// if new return val is passed, it is passed to the piped done
+								if (returnval && typeof returnval === 'function') {
+									returnval.promise().then(def.resolve, def.reject, def.notify);
+								}
+								else {	// if new return val is passed, it is passed to the piped done
 									def.resolve(returnval);
+								}
 							});
 						}
-						else
+						else {
 							deferred.done(def.resolve);
+						}
 					});
 
 					foreach(fail, function(func) {
-						if (typeof func == 'function') {
+						if (typeof func === 'function') {
 							deferred.fail(function() {
 								var returnval = func.apply(this, arguments);
-								if (returnval && typeof returnval == 'function')
+								
+								if (returnval && typeof returnval === 'function') {
 									returnval.promise().then(def.resolve, def.reject, def.notify);
-								else
+								} else {
 									def.reject(returnval);
+								}
 							});
 						}
-						else
+						else {
 							deferred.fail(def.reject);
+						}
 					});
 				}).promise();
 			}
@@ -187,30 +201,33 @@
 
 		deferred = {
 			resolveWith: function(context) {
-				if (status == 'pending') {
+				if (status === 'pending') {
 					status = 'resolved';
 					var args = resultArgs = (arguments.length > 1) ? arguments[1] : [];
-					for (var i = 0; i < doneFuncs.length; i++)
+					for (var i = 0; i < doneFuncs.length; i++) {
 						doneFuncs[i].apply(context, args);
+					}
 				}
 				return this;
 			},
 
 			rejectWith: function(context) {
-				if (status == 'pending') {
+				if (status === 'pending') {
 					status = 'rejected';
 					var args = resultArgs = (arguments.length > 1) ? arguments[1] : [];
-					for (var i = 0; i < failFuncs.length; i++)
+					for (var i = 0; i < failFuncs.length; i++) {
 						failFuncs[i].apply(context, args);
+					}
 				}
 				return this;
 			},
 
 			notifyWith: function(context) {
-				if (status == 'pending') {
+				if (status === 'pending') {
 					var args = resultArgs = (arguments.length > 1) ? arguments[1] : [];
-					for (var i = 0; i < progressFuncs.length; i++)
+					for (var i = 0; i < progressFuncs.length; i++) {
 						progressFuncs[i].apply(context, args);
+					}
 				}
 				return this;
 			},
@@ -230,8 +247,9 @@
 
 		var obj = promise.promise(deferred);
 
-		if (fn)
+		if (fn) {
 			fn.apply(obj, [obj]);
+		}
 
 		return obj;
 	}
@@ -239,7 +257,7 @@
 	D.when = function() {
 		if (arguments.length < 2) {
 			var obj = arguments.length ? arguments[0] : undefined;
-			if (obj && (typeof obj.isResolved == 'function' && typeof obj.isRejected == 'function')) {
+			if (obj && (typeof obj.isResolved === 'function' && typeof obj.isRejected === 'function')) {
 				return obj.promise();			
 			}
 			else {
@@ -255,8 +273,8 @@
 
 				for (var i = 0; i < args.length; i++) {
 					(function(j) {
-						args[j].done(function() { console.log('resolved'); rp[j] = (arguments.length < 2) ? arguments[0] : arguments; if (++done == size) { df.resolve.apply(df, rp); }})
-						.fail(function() { console.log('rejected'); df.reject(arguments); });
+						args[j].done(function() { rp[j] = (arguments.length < 2) ? arguments[0] : arguments; if (++done == size) { df.resolve.apply(df, rp); }})
+						.fail(function() { df.reject(arguments); });
 					})(i);
 				}
 
